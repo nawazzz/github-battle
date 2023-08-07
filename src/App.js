eecc088fa0ad6c4af23750129bbb1f57a84b9612
nawaz <khan.nawaz790@gmail.com>
2023-08-07 14:16:27 +0200

    add method for api call

--- src/App.js
+++ src/App.js
@@ -8,23 +8,42 @@ class App extends React.Component {
     super(props)
     this.state = {
       ghData: {},
-      inputValue: ''
+      inputValue: '',
+      handleButton: true
     }
   }
 
   componentDidMount() {
-    const url = "https://api.github.com/users/nawazzz"
-    fetch(url).then(res => res.json()).then(data => {
-      console.log(data)
-      this.setState({
-        ghData: data
-      })
-    })
+    // const url = "https://api.github.com/users/nawazzz"
+    // fetch(url).then(res => res.json()).then(data => {
+    //   console.log(data)
+    //   this.setState({
+    //     ghData: data 
+    //   })
+    // })
   }
 
   handleInputValue = (event) => {
     this.setState({
       inputValue: event.target.value
+    })
+  }
+
+  handleSubmitButton = (event) => {
+    if (this.state.inputValue) {
+      this.setState({
+        handleButton: false
+      }, ()=>{console.log(this.state)})
+      const url = `https://api.github.com/users/${this.state.inputValue}`
+      fetch(url).then(res => res.json()).then(data => {
+        // console.log(data)
+        this.setState({
+          ghData: data 
+        }, ()=>{console.log(this.state)})
+      })
+    }
+    this.setState({
+      inputValue: ''
     })
   }
 
@@ -41,7 +60,9 @@ class App extends React.Component {
         id="outlined-basic" value={this.state.inputValue} onChange={this.handleInputValue}
         label="Enter GitHub user" variant="outlined" />
       <Button variant="contained"
+        onClick={this.handleSubmitButton}
         style={{height: "55px"}}
+        
         >Submit</Button>
       </div>
       </React.Fragment>
