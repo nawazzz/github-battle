76a9bb9599088c8fc57c6776653217b71b4be771
nawaz <khan.nawaz790@gmail.com>
2023-08-09 10:33:14 +0200

    add second input with functionality

--- src/App.js
+++ src/App.js
@@ -8,8 +8,11 @@ class App extends React.Component {
     super(props)
     this.state = {
       ghData: {},
-      inputValue: '',
-      handleButton: true
+      ghDataSecondUser: {},
+      inputValueOne: '',
+      inputValueTwo: '',
+      handleButtonOne: true,
+      handleButtonTwo: true
     }
   }
 
@@ -23,48 +26,111 @@ class App extends React.Component {
     // })
   }
 
-  handleInputValue = (event) => {
+  handleinputValueOne = (event) => {
     this.setState({
-      inputValue: event.target.value
+      inputValueOne: event.target.value
     })
   }
 
-  handleSubmitButton = (event) => {
-    if (this.state.inputValue) {
+  handleSubmitButtonOne = (event) => {
+    if (this.state.inputValueOne) {
       this.setState({
-        handleButton: false
-      }, ()=>{console.log(this.state)})
-      const url = `https://api.github.com/users/${this.state.inputValue}`
+        handleButtonOne: !this.state.handleButtonOne
+      }, () => { console.log(this.state) })
+      const url = `https://api.github.com/users/${this.state.inputValueOne}`
       fetch(url).then(res => res.json()).then(data => {
         // console.log(data)
         this.setState({
-          ghData: data 
-        }, ()=>{console.log(this.state)})
+          ghData: data
+        }, () => { console.log(this.state) })
       })
     }
     this.setState({
-      inputValue: ''
+      inputValueOne: ''
+    })
+  }
+
+  handleUserOutput = (event) => {
+    this.setState({
+      handleButtonOne: !this.state.handleButtonOne
+    })
+  }
+
+  handleinputValueTwo = (event) => {
+    this.setState({
+      inputValueTwo: event.target.value
+    })
+  }
+
+  handleSubmitButtonTwo = (event) => {
+    console.log(event)
+    if (this.state.inputValueTwo) {
+      this.setState({
+        handleButtonTwo: !this.state.handleButtonTwo
+      }, () => { console.log(this.state) })
+      const url = `https://api.github.com/users/${this.state.inputValueTwo}`
+      fetch(url).then(res => res.json()).then(data => {
+        console.log(data)
+        this.setState({
+          ghDataSecondUser: data
+        }, () => { console.log(this.state) })
+      })
+    }
+    this.setState({
+      inputValueTwo: ''
+    })
+  }
+
+  handleUserOutputTwo = (event) => {
+    this.setState({
+      handleButtonTwo: !this.state.handleButtonTwo
     })
   }
 
   render() {
     return (
       <React.Fragment>
-      <div className="App">
-        <h1>Github Battle</h1>
-        <h2>User: {this.state.ghData.login}</h2>
-        <h3>Job: {this.state.ghData.bio}</h3>
-      </div>
-      <div>
-      <TextField 
-        id="outlined-basic" value={this.state.inputValue} onChange={this.handleInputValue}
-        label="Enter GitHub user" variant="outlined" />
-      <Button variant="contained"
-        onClick={this.handleSubmitButton}
-        style={{height: "55px"}}
-        
-        >Submit</Button>
-      </div>
+        <div className="App">
+          <h1>Github Battle</h1>
+          <h2>User: {this.state.ghData.login}</h2>
+          <h3>Job: {this.state.ghData.bio}</h3>
+        </div>
+        <div style={{display: 'flex', margin: '50px'}}>
+          <div style={{ display: this.state.handleButtonOne ? "block" : "none" }}>
+            <TextField
+              id="outlined-basic" value={this.state.inputValueOne} onChange={this.handleinputValueOne}
+              label="Enter GitHub user" variant="outlined" />
+            <Button variant="contained"
+              onClick={this.handleSubmitButtonOne}
+              style={{ height: "55px" }}
+            >Submit</Button>
+          </div>
+          <div style={{ display: this.state.handleButtonTwo ? "block" : "none" }}>
+            <TextField
+              id="outlined-basic" value={this.state.inputValueTwo} onChange={this.handleinputValueTwo}
+              label="Enter GitHub user" variant="outlined" />
+            <Button variant="contained"
+              onClick={this.handleSubmitButtonTwo}
+              style={{ height: "55px" }}
+            >Submit</Button>
+          </div>
+        </div>
+
+        <div style={{ display: !this.state.handleButtonOne ? "block" : "none", width: "200px", height: "50px" }}>
+          <img src={this.state.ghData.avatar_url} width={"100px"} />
+          <span><a href='https://github.com/nawazzz'>{this.state.ghData.login}</a></span>
+          <span onClick={this.handleUserOutput}
+            style={{ cursor: "pointer", fontWeight: "700" }}
+          >x</span>
+        </div>
+
+        <div style={{ display: !this.state.handleButtonTwo ? "block" : "none", width: "200px", height: "50px" }}>
+          <img src={this.state.ghDataSecondUser.avatar_url} width={"100px"} />
+          <span><a href='https://github.com/nawazzz'>{this.state.ghDataSecondUser.login}</a></span>
+          <span onClick={this.handleUserOutputTwo}
+            style={{ cursor: "pointer", fontWeight: "700" }}
+          >x</span>
+        </div>
       </React.Fragment>
     )
   }
