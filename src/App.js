2a713a162533716439e207a24ea764a749cf0ca5
nawaz <khan.nawaz790@gmail.com>
2023-08-09 11:21:35 +0200

    add results page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import BattleResult from './Battle-result';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,7 +13,8 @@ class App extends React.Component {
       inputValueOne: '',
       inputValueTwo: '',
       handleButtonOne: true,
-      handleButtonTwo: true
+      handleButtonTwo: true,
+      showResult: false
     }
   }
 
@@ -88,56 +90,77 @@ class App extends React.Component {
     })
   }
 
+  handleResults = (event) => {
+    this.setState({
+      showResult: true
+    })
+  }
+
   render() {
     return (
       <React.Fragment>
-        <div className="App">
-          <h1>Github Battle</h1>
-          <h2>User: {this.state.ghData.login}</h2>
-          <h3>Job: {this.state.ghData.bio}</h3>
-        </div>
-        <div style={{ display: 'flex', margin: '50px' }}>
-          <div style={{ display: this.state.handleButtonOne ? "block" : "none" }}>
-            <TextField
-              id="outlined-basic" value={this.state.inputValueOne} onChange={this.handleinputValueOne}
-              label="Enter GitHub user" variant="outlined" />
-            <Button variant="contained"
-              onClick={this.handleSubmitButtonOne}
-              style={{ height: "55px" }}
-            >Submit</Button>
-          </div>
-          <div style={{ display: this.state.handleButtonTwo ? "block" : "none" }}>
-            <TextField
-              id="outlined-basic" value={this.state.inputValueTwo} onChange={this.handleinputValueTwo}
-              label="Enter GitHub user" variant="outlined" />
-            <Button variant="contained"
-              onClick={this.handleSubmitButtonTwo}
-              style={{ height: "55px" }}
-            >Submit</Button>
-          </div>
-        </div>
-        <div style={{ display: 'flex' }}>
-          <div style={{ display: !this.state.handleButtonOne ? "block" : "none", width: "200px", height: "50px" }}>
-            <img src={this.state.ghData.avatar_url} width={"100px"} />
-            <span><a href='https://github.com/nawazzz'>{this.state.ghData.login}</a></span>
-            <span onClick={this.handleUserOutput}
-              style={{ cursor: "pointer", fontWeight: "700" }}
-            >x</span>
-          </div>
+        {this.state.showResult ?
+          (
+            <div>
+              <BattleResult ghData={this.state.ghData} ghDataSecondUser={this.state.ghDataSecondUser} inputValueOne={this.state.inputValueOne}
+                inputValueTwo={this.state.inputValueTwo} handleButtonOne={this.state.handleButtonOne} handleButtonTwo={this.state.handleButtonTwo}
+                showResult={this.state.showResult} />
+            </div>
+          ) : (
+            <div>
+              <div>
+                <div className="App">
+                  <h1>Github Battle</h1>
+                  <h2>User: {this.state.ghData.login}</h2>
+                  <h3>Job: {this.state.ghData.bio}</h3>
+                </div>
+                <div style={{ display: 'flex', margin: '50px' }}>
+                  <div style={{ display: this.state.handleButtonOne ? "block" : "none" }}>
+                    <TextField
+                      id="outlined-basic" value={this.state.inputValueOne} onChange={this.handleinputValueOne}
+                      label="Enter GitHub user" variant="outlined" />
+                    <Button variant="contained"
+                      onClick={this.handleSubmitButtonOne}
+                      style={{ height: "55px" }}
+                    >Submit</Button>
+                  </div>
+                  <div style={{ display: this.state.handleButtonTwo ? "block" : "none" }}>
+                    <TextField
+                      id="outlined-basic" value={this.state.inputValueTwo} onChange={this.handleinputValueTwo}
+                      label="Enter GitHub user" variant="outlined" />
+                    <Button variant="contained"
+                      onClick={this.handleSubmitButtonTwo}
+                      style={{ height: "55px" }}
+                    >Submit</Button>
+                  </div>
+                </div>
+                <div style={{ display: 'flex' }}>
+                  <div style={{ display: !this.state.handleButtonOne ? "block" : "none", width: "200px", height: "50px" }}>
+                    <img src={this.state.ghData.avatar_url} width={"100px"} />
+                    <span><a href='https://github.com/nawazzz'>{this.state.ghData.login}</a></span>
+                    <span onClick={this.handleUserOutput}
+                      style={{ cursor: "pointer", fontWeight: "700" }}
+                    >x</span>
+                  </div>
 
-          <div style={{ display: !this.state.handleButtonTwo ? "block" : "none", width: "200px", height: "50px" }}>
-            <img src={this.state.ghDataSecondUser.avatar_url} width={"100px"} />
-            <span><a href='https://github.com/nawazzz'>{this.state.ghDataSecondUser.login}</a></span>
-            <span onClick={this.handleUserOutputTwo}
-              style={{ cursor: "pointer", fontWeight: "700" }}
-            >x</span>
-          </div>
+                  <div style={{ display: !this.state.handleButtonTwo ? "block" : "none", width: "200px", height: "50px" }}>
+                    <img src={this.state.ghDataSecondUser.avatar_url} width={"100px"} />
+                    <span><a href='https://github.com/nawazzz'>{this.state.ghDataSecondUser.login}</a></span>
+                    <span onClick={this.handleUserOutputTwo}
+                      style={{ cursor: "pointer", fontWeight: "700" }}
+                    >x</span>
+                  </div>
 
-        </div>
+                </div>
+                <div>
+                  <button onClick={this.handleResults}
+                    style={{ display: !this.state.handleButtonOne && !this.state.handleButtonTwo ? 'block' : 'none', margin: '100px', cursor: 'pointer' }}
+                  >Battle</button>
+                </div>
+              </div>
+            </div>
+          )}
 
-
-        <button style={{ display: !this.state.handleButtonOne && !this.state.handleButtonTwo ? 'block' : 'none', margin: '100px', cursor: 'pointer' }}
-          >Battle</button>
       </React.Fragment>
     )
   }
---
+++ src/Battle-result.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class BattleResult extends React.Component {
+    render() {
+        return(
+            <div>
+                <div>
+                    Loser
+                </div>
+                <div>
+                    Winner
+                </div>
+            </div>
+        )
+    }
+}
+
+export default BattleResult;
